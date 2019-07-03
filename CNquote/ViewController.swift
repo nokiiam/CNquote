3687c792820047c3c62f305fd20aee5dcf01e007
Romain Mounier <romain.mounier@epita.fr>
2019-07-03 15:19:50 +0200

    Initial Commit

---
+++ CNquote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CNquote
+//
+//  Created by Romain on 03/07/2019.
+//  Copyright © 2019 Romain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
