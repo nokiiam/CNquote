982191e1c873b740b05077e4a85e8f1605d7ed6d
Romain Mounier <romain.mounier@epita.fr>
2019-07-03 16:15:34 +0200

    Initial architecture

---
+++ CNquote/LaunchSreen/LauchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LauchViewController.swift
+//  CNquote
+//
+//  Created by Romain on 03/07/2019.
+//  Copyright © 2019 Romain. All rights reserved.
+//
+
+import UIKit
+
+class LauchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CNquote/Service/APIService.swift
@@ -0,0 +1,9 @@
+//
+//  APIServices.swift
+//  CNquote
+//
+//  Created by Romain on 03/07/2019.
+//  Copyright © 2019 Romain. All rights reserved.
+//
+
+import Foundation
