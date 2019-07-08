5e6a4153b1a3721c3b0b939ea7aab616e63464f4
Adrien Hellec <adrien.hellec@epita.fr>
2019-07-08 18:28:57 +0200

    add asynchronous requests

--- CNquote/Quotes/QuotesTableViewController.swift
+++ CNquote/Quotes/QuotesTableViewController.swift
@@ -30,7 +30,7 @@ class QuotesTableViewController: UITableViewController {
             default:
                 self.api.loadMoreRandomQuote()
             }
-            self.tableView.reloadData()
+            DispatchQueue.main.async(execute: {self.tableView.reloadData()})
         })
         
     }
@@ -110,7 +110,7 @@ class QuotesTableViewController: UITableViewController {
                     default:
                         self.api.loadMoreRandomQuote()
                 }
-                self.tableView.reloadData()
+                DispatchQueue.main.async(execute: {self.tableView.reloadData()})
             })
         }
     }
@@ -122,7 +122,7 @@ class QuotesTableViewController: UITableViewController {
             if self.api.alreadyLoadedNewQuote.count == 0 {
                 DispatchQueue.global().async(execute: {
                     self.api.loadMoreNewQuote()
-                    self.tableView.reloadData()
+                    DispatchQueue.main.async(execute: {self.tableView.reloadData()})
                 })
             }
             self.navigationController?.navigationBar.topItem?.title = "Nouvelles"
@@ -134,7 +134,7 @@ class QuotesTableViewController: UITableViewController {
             if self.api.alreadyLoadedBestQuote.count == 0 {
                 DispatchQueue.global().async(execute: {
                     self.api.loadMoreBestQuote()
-                    self.tableView.reloadData()
+                    DispatchQueue.main.async(execute: {self.tableView.reloadData()})
                 })
             }
             self.navigationController?.navigationBar.topItem?.title = "Meilleures"
@@ -146,7 +146,7 @@ class QuotesTableViewController: UITableViewController {
             if self.api.alreadyLoadedRandomQuote.count == 0 {
                 DispatchQueue.global().async(execute: {
                     self.api.loadMoreRandomQuote()
-                    self.tableView.reloadData()
+                    DispatchQueue.main.async(execute: {self.tableView.reloadData()})
                 })
             }
             self.navigationController?.navigationBar.topItem?.title = "Aléatoires"
@@ -157,50 +157,5 @@ class QuotesTableViewController: UITableViewController {
         
         self.present(alert, animated: true, completion: nil)
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
